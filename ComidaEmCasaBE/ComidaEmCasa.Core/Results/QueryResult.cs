990e90ef73cf7133ca45a67b3a07b3bd39d89f2a
Miguel Bertemes Costa <miguelbertemes@hotmail.com>
2022-04-13 19:21:12 -0300

    Implemented QueryResult

---
+++ ComidaEmCasaBE/ComidaEmCasa.Core/Results/QueryResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ComidaEmCasa.Core.Results
+{
+    public class QueryResult<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public long TotalItems { get; set; } = 0;
+    }
+}
